0c2921180374764444200bea9660296db9d5d024
Geoff Schmit <gcschmit@gmail.com>
2020-10-05 11:01:51 -0500

    declared public interface for MileageTracker class

---
+++ MileageTracker.java
@@ -0,0 +1,93 @@
+
+/**
+ * This class models a mileage tracker for a car.
+ *
+ * @author gcschmit
+ * @version 27 September 2020
+ */
+public class MileageTracker
+{
+    /*
+     * 1. Define methods by specifying:
+     *      the visibility (e.g., public)
+     *      the return type (e.g., void)
+     *      the method name (e.g., incrementMilesDriven)
+     *      the parameters and their types (e.g., miles of type double)
+     */
+    
+    /**
+     * Increment the number of miles that the car has driven.
+     * 
+     * @param miles the additional distance, in miles, this car has driven
+     */
+    public void incrementMilesDriven(double miles)
+    {
+    }
+    
+    /**
+     * Returns the total number of miles driven
+     * 
+     * @return the total number of miles driven
+     */
+    public double getMilesDriven()
+    {
+        return 0.0;
+    }
+    
+    /**
+     * Increment the number of gallons of fuel that this car has consumed
+     * 
+     * @param gallons    the additional fuel, in gallons, this car has consumed
+     */
+    public void incrementFuelConsumed(double gallons)
+    {
+        
+    }
+    
+    /**
+     * Returns the total number of gallons of fuel consumed
+     * 
+     * @return the total number of gallons of fuel consumed
+     */
+    public double getFuelConsumed()
+    {
+        return 0.0;
+    }
+    
+    
+    /**
+     * Returns the current mileage, in miles per gallon
+     * 
+     * @return the current mileage, in miles per gallon
+     */
+    public double getMileage()
+    {
+        return 0.0;
+    }
+    
+    /**
+     * Returns the vehicle identification (VIN) of this car.
+     * 
+     * @return the vehicle identification (VIN) of this car
+     */
+    public String getVIN()
+    {
+        return null;
+    }
+    
+    /**
+     * Sets the vehicle identification (VIN) of this car.
+     * 
+     * @param newVIN    the vehicle identification (VIN) of this car
+     */
+    public void setVIN(String vin)
+    {
+        vin = vin;
+    }
+}
+
+
+
+
+
+
